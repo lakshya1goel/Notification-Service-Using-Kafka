b810c3c6f8fe99e43c847a0b7c6e377b82db0fd0
lakshya1goel <lakshya1234goel@gmail.com>
2025-07-29 21:35:39 +0530

    add producer & consumer for kafka
    Signed-off-by: lakshya1goel <lakshya1234goel@gmail.com>

---
+++ kafka/consumer.go
@@ -0,0 +1,50 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/lakshya1goel/Notification-Service-Using-Kafka.git/model"
+	"github.com/segmentio/kafka-go"
+)
+
+type KafkaConsumer interface {
+	Start(ctx context.Context)
+}
+
+type kafkaConsumer struct {
+	reader     *kafka.Reader
+}
+
+func NewKafkaConsumer(brokers []string, topic, groupID string) KafkaConsumer {
+	return &kafkaConsumer{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers:  brokers,
+			Topic:    topic,
+			GroupID:  groupID,
+			MinBytes: 10e3,
+			MaxBytes: 10e6,
+		}),
+	}
+}
+
+func (c *kafkaConsumer) Start(ctx context.Context) {
+	go func() {
+		for {
+			m, err := c.reader.ReadMessage(ctx)
+			if err != nil {
+				log.Println("Kafka read error:", err)
+				continue
+			}
+
+			var notif model.Notification
+			if err := json.Unmarshal(m.Value, &notif); err != nil {
+				log.Println("Unmarshal error:", err)
+				continue
+			}
+
+			log.Println("Received notification:", notif)
+		}
+	}()
+}
---
+++ kafka/producer.go
@@ -0,0 +1,44 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/lakshya1goel/Notification-Service-Using-Kafka.git/model"
+	"github.com/segmentio/kafka-go"
+)
+
+type KafkaProducer interface {
+	Produce(ctx context.Context, notif model.Notification) error
+}
+
+type kafkaProducer struct {
+	writer *kafka.Writer
+}
+
+func NewKafkaProducer(broker, topic string) KafkaProducer {
+	return &kafkaProducer{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(broker),
+			Topic:    topic,
+			Balancer: &kafka.LeastBytes{},
+		},
+	}
+}
+
+func (kp *kafkaProducer) Produce(ctx context.Context, notif model.Notification) error {
+	payload, err := json.Marshal(notif)
+	if err != nil {
+		return err
+	}
+
+	err = kp.writer.WriteMessages(ctx, kafka.Message{
+		Key:   []byte(notif.ID),
+		Value: payload,
+	})
+	if err != nil {
+		log.Println("Kafka write failed:", err)
+	}
+	return err
+}
